2b97b1418a63860278f18fdd490654dac09ad97c
m-onerl <monerl2525@gmail.com>
2026-02-28 19:04:00 +0100

    chore: add functionality of player run have to change animation for left and right walking

--- game.py
+++ game.py
@@ -1,10 +1,13 @@
 import pgzrun
 
+WIDTH = 800
+HEIGHT = 600
+
 game_state = 'menu'
 
 
 class Animation:
-    def __init__(self, frames, speed=0.2):
+    def __init__(self, frames, speed = 0.2):
         self.frames = frames
         self.speed = speed
         self.current_frame = 0
@@ -17,15 +20,61 @@ class Animation:
             self.current_frame = (self.current_frame + 1) % len(self.frames)
     
     def get_current_image(self):
-
         return self.frames[self.current_frame]
     
+    def reset(self):
+        self.current_frame = 0
+        self.timer = 0
+
  
 class Player:
-    #TODO how plater can move speed and checking borders of map
-    # coursor there is where w shot player still shooting
-    # can heal get some hp from floor
-    pass
+    def __init__(self, x, y):
+        self.walk_anim = Animation(['player_walk1', 'player_walk2', 'player_walk3', 'player_walk4'], speed = 0.15)
+        self.actor = Actor(self.walk_anim.get_current_image())
+        self.actor.x = x
+        self.actor.y = y
+        self.speed = 5
+        self.moving = False
+        self.facing_right = True
+    
+    def update(self, dt):
+        self.moving = False
+        
+        if keyboard.w:
+            self.actor.y -= self.speed
+            self.moving = True
+        if keyboard.s:
+            self.actor.y += self.speed
+            self.moving = True
+        if keyboard.a:
+            self.actor.x -= self.speed
+            self.moving = True
+            self.facing_right = False
+        if keyboard.d:
+            self.actor.x += self.speed
+            self.moving = True
+            self.facing_right = True
+
+        self.actor.x = max(20, min(WIDTH - 20, self.actor.x))
+        self.actor.y = max(20, min(HEIGHT - 20, self.actor.y))
+ 
+        if self.moving:
+            self.walk_anim.update(dt)
+            self.actor.image = self.walk_anim.get_current_image()
+
+        if self.facing_right:
+            self.actor.angle = 0
+        else:
+            self.actor.angle = 0  
+            
+    def draw(self):
+        if self.facing_right:
+            self.actor.draw()
+        else:
+            screen.blit(self.actor.image, (self.actor.x - self.actor.width//2, self.actor.y - self.actor.height//2))
+
+
+player = Player(400, 300)
 
 class Zombie:
     #TODO randomize of  moving zombie to catch player but have to make some wreid move random move for every one and sometimes dodges
@@ -33,10 +82,6 @@ class Zombie:
     pass
 
 
-walk_anim = Animation(["player_walk1", "player_walk2"], speed=0.15)
-player = Actor(walk_anim.get_current_image())
-player.x = 400
-player.y = 300
 
 def draw():
     screen.fill((20, 20, 30))
@@ -53,9 +98,8 @@ def draw():
 # function for update of animation
 def update(dt):
     if game_state == "game":
-        walk_anim.update(dt)
-        player.image = walk_anim.get_current_image()
-
+        player.update(dt)
+        
 def on_key_down(key):
     global game_state
     if game_state == "menu" and key == keys.RETURN:
