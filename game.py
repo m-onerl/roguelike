c4fa528774eabdb84070b9441a9554d6b1c57890
m-onerl <monerl2525@gmail.com>
2026-02-28 17:59:19 +0100

    chore: initialize project structure and asset folders

---
+++ game.py
@@ -0,0 +1,19 @@
+from pygame import React
+import pgzrun
+
+class Animation:
+    #TODO Here have to implement base class for animations of moving player/enemy
+    pass
+
+class Player:
+    #TODO how plater can move speed and checking borders of map
+    # coursor there is where w shot player still shooting
+    # can heal get some hp from floor
+    pass
+
+class Zombie:
+    #TODO randomize of  moving zombie to catch player but have to make some wreid move random move for every one and sometimes dodges
+    # about 40 zombies respown one stronger one have X2 hp and more dmg like 30%
+    pass
+
+
